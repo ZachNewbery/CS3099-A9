210dc5760b7668c36093eda623ac5f21bd472f1e
Anson Cheung <twac@st-andrews.ac.uk>
2020-10-16 23:54:41 +0800

    Add very basic demo server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, HttpServer, App, middleware, web, Result};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    let bind = format!("{}:{}",
+                       std::env::var("BIND_ADDRESS").expect("BIND_ADDRESS"),
+                       std::env::var("BIND_PORT").expect("BIND_PORT")
+    );
+
+    println!("Starting server on: {}", &bind);
+
+    // Start the server!
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(hello)
+    })
+        .bind(bind)?
+        .run()
+        .await
+}
+
+#[get("/hello/{name}")]
+async fn hello(web::Path(name): web::Path<String>) -> Result<String> {
+    Ok(
+        format!("Hello {}", name)
+    )
 }
