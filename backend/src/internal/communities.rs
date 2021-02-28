e786b458e4253fad8c6c7158a4cfe19e958697cb
Anson Cheung <twac@st-andrews.ac.uk>
2021-02-28 16:01:05 +0800

    Add internal community routes

---
+++ backend/src/internal/communities.rs
@@ -0,0 +1,64 @@
+use actix_web::{delete, get, post, web, patch, HttpRequest, HttpResponse, Result};
+use serde::{Serialize, Deserialize};
+use crate::DBPool;
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize)]
+pub struct ListCommunities {
+    host: Option<String>,
+}
+
+#[get("/communities")]
+pub(crate) async fn list_communities(
+    _pool: web::Data<DBPool>,
+    _request: HttpRequest,
+    _specification: web::Json<ListCommunities>
+) -> Result<HttpResponse> {
+    // TODO: Implement /internal/communities (GET)
+
+    // Return type: in discussion
+    unimplemented!()
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CreateCommunity {
+    id: String,
+    title: String,
+    description: String,
+}
+
+#[post("/communities/create")]
+pub(crate) async fn create_community(
+    _pool: web::Data<DBPool>,
+    _request: HttpRequest,
+    _specification: web::Json<CreateCommunity>
+) -> Result<HttpResponse> {
+    // TODO: Implement /internal/communities/create (POST)
+
+    // Return type: none
+    unimplemented!()
+}
+
+#[delete("/communities/{id}")]
+pub(crate) async fn delete_community(
+    _pool: web::Data<DBPool>,
+    _request: HttpRequest,
+    web::Path(_id): web::Path<Uuid>,
+) -> Result<HttpResponse> {
+    // TODO: Implement /internal/communities/id (DELETE)
+
+    // Return type: none
+    unimplemented!()
+}
+
+#[patch("/communities/{id}")]
+pub(crate) async fn edit_community_details(
+    _pool: web::Data<DBPool>,
+    _request: HttpRequest,
+    web::Path(_id): web::Path<Uuid>,
+) -> Result<HttpResponse> {
+    // TODO: Implement /internal/communities/id (PATCH)
+
+    // Return type: none
+    unimplemented!()
+}
--- backend/src/internal/mod.rs
+++ backend/src/internal/mod.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 pub mod authentication;
 pub mod posts;
 pub mod user;
+pub mod communities;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum LocatedCommunity {
