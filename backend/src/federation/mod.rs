c297fe3642cb0467950189494b7f679a87f3ea2b
zn6 <zn6@st-andrews.ac.uk>
2021-03-31 12:21:01 +0100

    make fed/key non-static

--- backend/src/federation/mod.rs
+++ backend/src/federation/mod.rs
@@ -1,5 +1,6 @@
 use crate::internal::authentication::request_wrapper;
 use actix_web::{get, http, web, HttpResponse, Result};
+use std::fs;
 
 #[get("/hello/{name}")]
 pub async fn hello(web::Path(name): web::Path<String>) -> Result<String> {
@@ -11,14 +12,7 @@ pub(crate) async fn key() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok()
         .header(http::header::CONTENT_TYPE, "application/x-pem-file")
         .body(
-            "-----BEGIN PUBLIC KEY-----\n
-    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxhGEP9mv/joN3UtyV0XN
-    Bgwn4N9wKB7N0IDR4dN+JHqmTitUUSbm8h3wCxScbAWhhNgrqHf61kbfmnbjKf3x
-    /uj5dj1t6PuGudjInB+T5/ADw01YpjF/ASt6WqB92ch3QGvaHiomVVz5yWPQzYzz
-    R6DeA8GU4f6Ha4+T8fZDAqF1dggVILzm+uZNMy7tM/8qmo2dWVgD+wF1Yjhp9PWl
-    bNyJ+6hx2M0PukADlXSEaVZw4grYCSIU2SyWsNCboOdZ8qsCAwqRT3K8RunBUVgk
-    2/p13iBQz+E7s7dIdOKXJKtLFtnNelZsZ9uGl9ZeJS28yR2LnhZZ/LIwzMv6RINP
-    OwIDAQAB\n-----END PUBLIC KEY-----",
+            fs::read_to_string("fed_auth_pub.pem")?,
         ))
 }
 
