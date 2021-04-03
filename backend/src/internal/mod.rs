98634a7544fc91ce11bc9a6c6655538c9b4e46e7
zn6 <zn6@st-andrews.ac.uk>
2021-04-03 14:07:51 +0100

    Add api/servers

--- backend/src/internal/mod.rs
+++ backend/src/internal/mod.rs
@@ -1,4 +1,6 @@
+use actix_web::{get, http, HttpResponse, Result};
 use serde::{Deserialize, Serialize};
+use std::fs;
 
 pub mod authentication;
 pub mod communities;
@@ -10,4 +12,15 @@ pub mod user;
 pub enum LocatedCommunity {
     Local { id: String },
     Federated { id: String, host: String },
+}
+
+#[get("/servers")]
+pub(crate) async fn discover() -> Result<HttpResponse> {
+    let file = fs::File::open("known_hosts.txt").expect("file should open read only");
+    let json: serde_json::Value =
+        serde_json::from_reader(file).expect("file should be proper JSON");
+
+    Ok(HttpResponse::Ok()
+        .header(http::header::CONTENT_TYPE, "application/json")
+        .body(json))
 }
